e6c0d7ba9c3265db962c5201019987dcba4f4272
MertcanYildirim078 <99068314@mydavinci.nl>
2022-03-07 11:39:53 +0100

    'Javalightklaar'

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'Lightswitch on';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+function button(event){
+    console.dir(event)
+    if(document.body.style.backgroundColor == "black"){ //== is soort check en = verander
+        document.body.style.backgroundColor = "white"
+        btn.innerHTML = 'Lightswitch on'
+        console.log('Lightswitch on')
 
+    } else{
+        document.body.style.backgroundColor = "black"
+        btn.innerHTML = 'Lightswitch off'
+        console.log('Lightswitch off')
+    }
+                                                 //document.body.style.backgroundColor = "green"
+                                                 //btn.style.backgroundColor = 'red'
+}
+
+btn.onclick = button;
 
 
 // schijf hier tussen je code
